d6cdb9dfa3698fd6573ebf04a81ea223cbdb98a6
ILliTAH <pepsimanzeal@gmail.com>
2017-06-07 12:40:21 +0700

    Change form1

---
+++ AutoLog/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace AutoLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_login = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.user = new System.Windows.Forms.TextBox();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_login
+            // 
+            this.button_login.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_login.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_login.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_login.Location = new System.Drawing.Point(685, 156);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(110, 39);
+            this.button_login.TabIndex = 0;
+            this.button_login.Text = "LOGIN";
+            this.button_login.UseVisualStyleBackColor = false;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(2, 2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(641, 510);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Url = new System.Uri("http://logoutwifi.net/login", System.UriKind.Absolute);
+            // 
+            // user
+            // 
+            this.user.Font = new System.Drawing.Font("TH Sarabun New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.user.Location = new System.Drawing.Point(660, 89);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(155, 33);
+            this.user.TabIndex = 2;
+            this.user.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pass
+            // 
+            this.pass.Font = new System.Drawing.Font("TH Sarabun New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pass.Location = new System.Drawing.Point(660, 128);
+            this.pass.Name = "pass";
+            this.pass.PasswordChar = '●';
+            this.pass.Size = new System.Drawing.Size(155, 22);
+            this.pass.TabIndex = 3;
+            this.pass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("1Yued", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(659, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 68);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ฟูลเฮ้าส์แมนชั่น";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(705, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "OFFLINE";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(692, 195);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 32);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Auto login";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("TH Sarabun New", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(661, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "PING CHECK STATUS";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "FullHouse Auto Login";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(672, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "INTERNET AUTOLOGIN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(827, 511);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button_login);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(843, 550);
+            this.MinimumSize = new System.Drawing.Size(843, 550);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AutoLog/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoLog
+{
+    public partial class Form1 : Form
+    {
+
+        public string username;
+        public string password;
+        public string host = "google.com";
+        public int timeout = 2000;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            this.Text = "FullHouse Auto Login : HANDDEV";
+
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+
+            username = user.Text;
+            password = pass.Text;
+
+
+            webBrowser1.Navigate("http://logoutwifi.net/login?username="+username+ "&password="+password);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            Ping myping = new Ping();
+            
+            byte[] buffer = new byte[32];
+
+            
+
+            PingOptions pingOption = new PingOptions();
+            PingReply reply = myping.Send(host, timeout, buffer, pingOption);
+            if(reply.Status == IPStatus.Success)
+            {
+               
+                    label2.BackColor = Color.LimeGreen;
+                    label2.Text = "ONLINE";
+                
+
+
+
+
+            }
+            else
+            {
+                if (checkBox1.Checked)
+                {
+                    username = user.Text;
+                    password = pass.Text;
+                    webBrowser1.Navigate("http://logoutwifi.net/login?username=" + username + "&password=" + password);
+
+                }
+                label2.BackColor = Color.Red;
+                label2.Text = "OFFLINE";
+              
+                
+
+            }
+
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Minimized)
+            {
+
+                Hide();
+                notifyIcon1.Visible = true;
+
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+    }
+}
